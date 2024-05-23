d93c17f250cef49a55b44917f55cb7e88e583fc9
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2024-05-23 14:44:15 +0200

    Delete code_src/Théo-code/src/components/RealTimeIntensityValues.js

--- code_src/Théo-code/src/components/RealTimeIntensityValues.js
+++
@@ -1,8 +0,0 @@
-// RealTimeIntensityValues.js
-import React from 'react';
-
-function RealTimeIntensityValues() {
-  // Code pour afficher les valeurs de l’intensité en temps réel
-}
-
-export default RealTimeIntensityValues;
