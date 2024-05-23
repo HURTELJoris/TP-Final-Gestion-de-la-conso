d39d9792b2f4780bb106ddefb082015fec92995b
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2024-05-23 14:46:08 +0200

    Delete code_src/Théo-code/src/components/UserPowerTracking.js

--- code_src/Théo-code/src/components/UserPowerTracking.js
+++
@@ -1,8 +0,0 @@
-// UserPowerTracking.js
-import React from 'react';
-
-function UserPowerTracking() {
-  // Code pour avoir un suivi de puissance reçu par les panneaux par utilisateur
-}
-
-export default UserPowerTracking;
