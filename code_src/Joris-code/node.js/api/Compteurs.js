25b62a2dbbcee0cac3a170d0ca6666be51c51175
Joris <113522406+HURTELJoris@users.noreply.github.com>
2024-05-23 11:08:31 +0200

    Update V6.0 - Node.js
    - Réduction de la consommation de mémoire
    - Optimisation du code de Compteurs.js
    - Dissimulation d'informations sensibles
    - Ajout de la partie récupération des données C++ du capteur de luminosité
    - Et d'autres fonctionnalités moins importantes

--- code_src/Joris-code/node.js/api/Compteurs.js
+++ code_src/Joris-code/node.js/api/Compteurs.js
@@ -11,19 +11,31 @@ class Compteurs {
     // Constructeur de la classe Simulateur.
     constructor() {
         this.proportionsTempVertStockees = [];
-        // Taille maximale du tableau de toutes les proportion de temps vert stockées. Ajustable au choix et déprendra de l'abonnement de temps pris par l'utilisateur (demander HUGO).
+        // Taille maximale du tableau de toutes les proportion de temps vert stockées. Ajustable au choix.
         this.MAX_SIZE = 10;
         this.previousSourceVerte = null;
         this.previoustabPowerBox = null;
+        this.config = this.loadConfig();
+    }
 
-        // Fichier qui gère les adresses sur lesquelles POST/GET
-        fs.readFile("./config.json", "utf8", (error, data) => {
-            if (error) console.log(error);
-            assert(!error); // Quitte si erreur de chargement du fichier de configuration.
-            this.config = JSON.parse(data);
+    //////////////////////////////////////////////////////////////////////////
 
-            //console.log(this.config);
-        });
+    /**
+     * Charge les configurations à partir d'un fichier JSON.
+     *
+     * Lit le fichier 'config.json', parse son contenu et retourne les données sous forme d'objet.
+     * En cas d'erreur, log une erreur et retourne un objet vide.
+     *
+     * @returns {Object} - Un objet contenant les configurations lues.
+     */
+    loadConfig() {
+        try {
+            const data = fs.readFileSync('config.json', 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            console.error('Error reading config file:', err);
+            return {};
+        }
     }
 
     //////////////////////////////////////////////////////////////////////////
@@ -36,7 +48,8 @@ class Compteurs {
     async ecouterDonneesCpp() {
         // Création d'un serveur socket pour écouter les données provenant du code C++
         const server = net.createServer((socket) => {
-            console.log('Connexion socket établie avec le code C++');
+            console.log('');
+            console.log('Connexion socket établie avec les compteurs');
 
             // Gestion des données reçues du code C++
             let dataReceived = '';
@@ -48,41 +61,56 @@ class Compteurs {
                 const parsedData = jsonData.map(line => JSON.parse(line));
 
                 //console.log(parsedData.length);
-                //console.log('Données reçues du code C++ :', dataReceived);
+                //console.log('Données reçues des compteurs :', dataReceived);
 
                 dataReceived = '';
 
                 for (const data of parsedData) {
                     // Vérifier si toutes les données ont été reçues. Si toutes les données sont reçues, alors on les affiche et on les envoie au programme principal.
-                    if (data.sourceVerte !== null && data.tabPowerBox !== null && Object.values(data.tabPowerBox).every(value => value !== null)) {
+                    if (data.sourceVerte !== undefined && data.tabPowerBox !== undefined && /*Object.values(data.tabPowerBox).every(value => value !== undefined)*/ data.tabPowerBox !== undefined) {
 
                         data.sourceVerte = Number(data.sourceVerte);
                         data.tabPowerBox = Object.values(data.tabPowerBox).map(Number);
 
-                        console.log('Données reçues du code C++ :');
+                        console.log('Données reçues des compteurs :');
                         console.table(data);
+                        //this.boucle(data);
+                    }
+
+                    if (data.luminosity !== undefined) {
+
+                        data.luminosity = Number(data.luminosity);
+
+                        console.log('Données reçues du capteur :');
+                        console.table(data);
+                        //this.boucle(data);
+                    }
+
+                    if(data.sourceVerte !== undefined || data.tabPowerBox !== undefined || /*Object.values(data.tabPowerBox).every(value => value !== undefined)*/ data.tabPowerBox !== undefined || data.luminosity !== undefined)
+                    {
+                        console.log('Envoi des données :');
                         this.boucle(data);
                     }
                 }
             });
 
 
-            // Gestion de la fermeture de la connexion socket par le code C++
+            // Gestion de la fermeture de la connexion socket par les compteurs
             socket.on('end', () => {
-                console.log('Connexion socket fermée par le code C++');
+                console.log('Connexion socket fermée par les compteurs');
             });
 
             // Gestion des erreurs lors de la communication socket
             socket.on('error', (error) => {
 
-                console.error('Erreur lors de la communication socket avec le code C++ :', error.message);
+                console.error('Un client s\'est déconnecté :', error.message);
 
             });
         });
 
         // Écoute des connexions socket entrantes du code C++
-        server.listen(1234, '192.168.64.88', () => {
-            console.log('Serveur socket en écoute sur l\'adresse IP 192.168.64.88 et le port 1234');
+        server.listen(this.config.PortNode_js, this.config.IPNode_js, () => {
+            console.log('Serveur socket en écoute sur l\'adresse IP', this.config.IPNode_js ,'et le port', JSON.stringify(this.config.PortNode_js));
         });
 
         // Gestion de la fermeture du serveur socket
@@ -101,10 +129,12 @@ class Compteurs {
      * @returns {Promise<void>} - Une promesse qui se résout lorsque la requête HTTP est terminée.
      */
     async SendBoxDataToAPI(config, data) {
+        //console.log(data);
+        //console.log(config);
         try {
             // Envoi d'une requête POST à l'API avec les données de box
             const response = await axios.post(config, data); // Changer le chemin lorsque Simon et Hassan auront fait le code pour la box
-
+        
             console.log('Réponse de l\'API :', response.data);
         } catch (error) {
             console.error('Erreur lors de la communication avec l\'API :', error.message);
@@ -166,94 +196,136 @@ class Compteurs {
         try {
 
             //console.log('data :',dataCPP);
-            //console.log('data.sourceVerte', dataCPP.sourceVerte);
-            const sourceVerte = dataCPP.sourceVerte;
-            const tabPowerBox = dataCPP.tabPowerBox;
-            const date = dataCPP.date;
 
-            //console.log(typeof (sourceVerte));
+            if(dataCPP.luminosity !== undefined && dataCPP.date !== undefined)
+            {
+                const luminosity = dataCPP.luminosity;
+                const date = dataCPP.date;
 
-            /* 
-                console.log(``);
-                console.log(`Valeur de sourceVerte : ${sourceVerte}`);
-                console.log(`Valeurs de tabPowerBox : ${JSON.stringify(tabPowerBox)}`);
-                console.log(``);
-            */
+                /*
+                console.log('dataCPP.luminosity',dataCPP.luminosity);
+                console.log('typeof (luminosity)',typeof (luminosity));
+                console.log('luminosity', luminosity);
 
-            const proportionsTempVert = this.calculerProportionTempVert(sourceVerte, tabPowerBox);
-            console.log(``);
-            console.log(`Proportions de temps vert : ${JSON.stringify(proportionsTempVert)}`);
-            console.log(``);
-            console.log(`Tableau de proportions : ${JSON.stringify(this.proportionsTempVertStockees)}`);
-            console.log(``);
-
-
-            const moyennesProportions = new Array(8).fill(0);
-            for (let i = 0; i < this.proportionsTempVertStockees.length; i++) {
-                for (let j = 0; j < this.proportionsTempVertStockees[i].length; j++) {
-                    moyennesProportions[j] += this.proportionsTempVertStockees[i][j];
-                }
-            }
-            for (let i = 0; i < moyennesProportions.length; i++) {
-                moyennesProportions[i] /= this.proportionsTempVertStockees.length;
-                moyennesProportions[i] = parseFloat(moyennesProportions[i].toFixed(2));
-            }
-
-            console.log(`Moyennes des proportions de temps vert : ${JSON.stringify(moyennesProportions)}`);
-            console.log(``);
-
-            /* */
-
-            // Vérifier si la valeur de sourceVerte a changé. Si oui, on ajoute les données en BDD grâce à l'API de Simon.
-            if (this.previousSourceVerte !== null && this.previousSourceVerte !== sourceVerte) {
-
-                console.log(`La valeur de sourceVerte a changé : ${this.previousSourceVerte} -> ${sourceVerte}`);
+                console.log('dataCPP.date', dataCPP.date);
+                console.log('typeof (date)',typeof (date));
+                console.log('date', date);
+                */
 
                 // Création du tableau de données à insérer dans l'API
                 const dataAPI = [];
 
                 dataAPI.push({
-                    powerbox: JSON.stringify(tabPowerBox),
-                    source_verte: sourceVerte,
-                    date: date,
-                    proportion_temp_vert: JSON.stringify(moyennesProportions)
+                    luminosite: luminosity,
+                    date: date
                 });
                 //console.log(dataAPI[0]);
                 //console.table(dataAPI);
 
                 // Appel de la méthode SendBoxDataInAPIToBDD avec le tableau de données en paramètre
-                await this.SendBoxDataToAPI(this.config.APICallbackURLBDD, dataAPI);
-            } else {
-                console.log(``);
+                await this.SendBoxDataToAPI(this.config.APICallbackURLuminosite, dataAPI);
             }
 
 
-            // Vérifier si une des valeurs parmis toutes les valeurs a changé. Si oui, envoie les données à l'API de Hassan (Groupe Gestion Accès Abris).
-            if ((this.previousSourceVerte !== null && this.previousSourceVerte !== sourceVerte) || (this.previoustabPowerBox !== null && this.previoustabPowerBox !== tabPowerBox)) {
+            if(dataCPP.sourceVerte !== undefined && Object.values(dataCPP.tabPowerBox).every(value => value !== undefined && dataCPP.date !== undefined))
+            {
+                //console.log('typeof (sourceVerte)', typeof (sourceVerte));
+                //console.log('data.sourceVerte', dataCPP.sourceVerte);
+                //console.log('data.sourceVerte', dataCPP.sourceVerte !== null);
+                
+                const sourceVerte = dataCPP.sourceVerte;
+                const tabPowerBox = dataCPP.tabPowerBox;
+                const date = dataCPP.date;
 
-                console.log(`Une des valeurs a changé : ${this.previousSourceVerte} -> ${sourceVerte}`);
-                console.log(`Une des valeurs a changé : ${JSON.stringify(this.previoustabPowerBox)} -> ${JSON.stringify(tabPowerBox)}`);
+                /* 
+                    console.log(``);
+                    console.log(`Valeur de sourceVerte : ${sourceVerte}`);
+                    console.log(`Valeurs de tabPowerBox : ${JSON.stringify(tabPowerBox)}`);
+                    console.log(``);
+                */
 
-                // Création du tableau de données à insérer dans l'API
-                const dataAPIAccesAbri = [];
-
-                dataAPIAccesAbri.push({
-                    PuissanceVerte: sourceVerte,
-                    id_statut_box: JSON.stringify(tabPowerBox),
-                    date: date
-                });
-                //console.log(dataAPIAccesAbri[0]);
-                //console.table(dataAPIAccesAbri);
-
-                // Appel de la méthode SendBoxDataInAPIAccesAbri avec le tableau de données en paramètre
-                await this.SendBoxDataToAPI(this.config.APICallbackURLAccesAbris, dataAPIAccesAbri);
-            } else {
+                const proportionsTempVert = this.calculerProportionTempVert(sourceVerte, tabPowerBox);
                 console.log(``);
+                console.log(`Proportions de temps vert : ${JSON.stringify(proportionsTempVert)}`);
+                console.log(``);
+                console.log(`Tableau de proportions : ${JSON.stringify(this.proportionsTempVertStockees)}`);
+                console.log(``);
+
+
+                const moyennesProportions = new Array(8).fill(0);
+                for (let i = 0; i < this.proportionsTempVertStockees.length; i++) {
+                    for (let j = 0; j < this.proportionsTempVertStockees[i].length; j++) {
+                        moyennesProportions[j] += this.proportionsTempVertStockees[i][j];
+                    }
+                }
+                for (let i = 0; i < moyennesProportions.length; i++) {
+                    moyennesProportions[i] /= this.proportionsTempVertStockees.length;
+                    moyennesProportions[i] = parseFloat(moyennesProportions[i].toFixed(2));
+                }
+
+                console.log(`Moyennes des proportions de temps vert : ${JSON.stringify(moyennesProportions)}`);
+                console.log(``);
+
+                // Vérifier si la valeur de sourceVerte a changé. Si oui, on ajoute les données en BDD grâce à l'API de Simon.
+                if (this.previousSourceVerte !== null && this.previousSourceVerte !== sourceVerte && this.previoustabPowerBox !== null) {
+
+                    console.log(`La valeur de sourceVerte a changé : ${this.previousSourceVerte} -> ${sourceVerte}`);
+
+                    // Création du tableau de données à insérer dans l'API
+                    const dataAPI = [];
+
+                    dataAPI.push({
+                        powerbox: JSON.stringify(tabPowerBox),
+                        source_verte: sourceVerte,
+                        date: date,
+                        proportion_temp_vert: JSON.stringify(moyennesProportions)
+                    });
+                    //console.log(dataAPI[0]);
+                    //console.table(dataAPI);
+
+                    // Création du tableau de données à insérer dans l'API
+                    const dataAPIAccesAbri = [];
+
+                    dataAPIAccesAbri.push({
+                        PuissanceVerte: sourceVerte,
+                        id_statut_box: JSON.stringify(tabPowerBox),
+                        date: date
+                    });
+                    //console.log(dataAPIAccesAbri[0]);
+                    //console.table(dataAPIAccesAbri);
+
+                    // Appel de la méthode SendBoxDataInAPIToBDD avec le tableau de données en paramètre
+                    await this.SendBoxDataToAPI(this.config.APICallbackURLBDD, dataAPI);
+                    // Appel de la méthode SendBoxDataInAPIAccesAbri avec le tableau de données en paramètre
+                    await this.SendBoxDataToAPI(this.config.APICallbackURLAccesAbris, dataAPIAccesAbri);
+                } 
+                else if ((this.previousSourceVerte !== null && this.previousSourceVerte !== sourceVerte) || (this.previoustabPowerBox !== null && this.previoustabPowerBox !== tabPowerBox))
+                {
+                    console.log(`Une des valeurs a changé : ${this.previousSourceVerte} -> ${sourceVerte}`);
+                    console.log(`Une des valeurs a changé : ${JSON.stringify(this.previoustabPowerBox)} -> ${JSON.stringify(tabPowerBox)}`);
+
+                    // Création du tableau de données à insérer dans l'API
+                    const dataAPIAccesAbri = [];
+
+                    dataAPIAccesAbri.push({
+                        PuissanceVerte: sourceVerte,
+                        id_statut_box: JSON.stringify(tabPowerBox),
+                        date: date
+                    });
+                    //console.log(dataAPIAccesAbri[0]);
+                    //console.table(dataAPIAccesAbri);
+
+                    // Appel de la méthode SendBoxDataInAPIAccesAbri avec le tableau de données en paramètre
+                    await this.SendBoxDataToAPI(this.config.APICallbackURLAccesAbris, dataAPIAccesAbri);
+                }
+                else {
+                    console.log(``);
+                }
+
+                this.previousSourceVerte = sourceVerte;
+                this.previoustabPowerBox = tabPowerBox;
             }
-
-            this.previousSourceVerte = sourceVerte;
-            this.previoustabPowerBox = tabPowerBox;
-
+            
         } catch (error) {
             console.error('Erreur lors de la réception des données provenant du code C++ :', error.message);
             return;
