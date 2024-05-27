d39c690b8ffc4fea65dd43cae92159276e717ade
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2024-05-27 14:53:23 +0200

    Delete code_src/Théo-code/src/components/EnergyProductionChart.js

--- code_src/Théo-code/src/components/EnergyProductionChart.js
+++
@@ -1,8 +0,0 @@
-// EnergyProductionChart.js
-import React from 'react';
-
-function EnergyProductionChart() {
-  // Code pour afficher les courbes de production d’énergie du panneau
-}
-
-export default EnergyProductionChart;
